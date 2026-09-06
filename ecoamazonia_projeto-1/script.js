0273f7db2fb846a2ddd0b77eeb397fa85206f0b8
LevizBlessedpo <marcosmcod@gmail.com>
2026-09-06 17:32:57 -0300

    HTML arrumado

--- ecoamazonia_projeto-1/script.js
+++ ecoamazonia_projeto-1/script.js
@@ -1,18 +1,3 @@
-// Dados do site de desmatamento do Brasil Escola
-const dadosProdes = [
-    { ano: '1991', taxa: 11030 }, { ano: '1992', taxa: 13786 }, { ano: '1993', taxa: 14896 },
-    { ano: '1994', taxa: 14896 }, { ano: '1995', taxa: 29059 }, { ano: '1996', taxa: 18161 },
-    { ano: '1997', taxa: 13227 }, { ano: '1998', taxa: 17383 }, { ano: '1999', taxa: 17259 },
-    { ano: '2000', taxa: 18226 }, { ano: '2001', taxa: 18165 }, { ano: '2002', taxa: 21650 },
-    { ano: '2003', taxa: 25396 }, { ano: '2004', taxa: 27772 }, { ano: '2005', taxa: 19014 },
-    { ano: '2006', taxa: 14286 }, { ano: '2007', taxa: 11651 }, { ano: '2008', taxa: 12911 },
-    { ano: '2009', taxa: 7464 },  { ano: '2010', taxa: 7000 },  { ano: '2011', taxa: 6418 },
-    { ano: '2012', taxa: 4571 },  { ano: '2013', taxa: 5891 },  { ano: '2014', taxa: 5012 },
-    { ano: '2015', taxa: 6207 },  { ano: '2016', taxa: 7893 },  { ano: '2017', taxa: 6947 },
-    { ano: '2018', taxa: 7536 },  { ano: '2019', taxa: 10129 }, { ano: '2020', taxa: 11088 },
-    { ano: '2021', taxa: 13038 }
-];
-
 // ===== FOLHAS CAINDO NO FUNDO (DECORATIVO) =====
 function criarFolhas(quantidade = 20) {
     const container = document.getElementById('folhasContainer');
@@ -45,10 +30,6 @@ document.addEventListener('DOMContentLoaded', () => {
     criarFolhas(25); 
 });
 
-
-const anos = dadosProdes.map(item => item.ano);
-const taxas = dadosProdes.map(item => item.taxa);
-
 //Config para deixar o site em light mode & dark mode
 const TemaSite= document.getElementById('btnTema');
 
@@ -65,6 +46,25 @@ btnTema.addEventListener('click', () => {
         btnTema.style.color = '#000';
     }
 });
+
+// Dados do site de desmatamento do Brasil Escola
+const dadosProdes = [
+    { ano: '1991', taxa: 11030 }, { ano: '1992', taxa: 13786 }, { ano: '1993', taxa: 14896 },
+    { ano: '1994', taxa: 14896 }, { ano: '1995', taxa: 29059 }, { ano: '1996', taxa: 18161 },
+    { ano: '1997', taxa: 13227 }, { ano: '1998', taxa: 17383 }, { ano: '1999', taxa: 17259 },
+    { ano: '2000', taxa: 18226 }, { ano: '2001', taxa: 18165 }, { ano: '2002', taxa: 21650 },
+    { ano: '2003', taxa: 25396 }, { ano: '2004', taxa: 27772 }, { ano: '2005', taxa: 19014 },
+    { ano: '2006', taxa: 14286 }, { ano: '2007', taxa: 11651 }, { ano: '2008', taxa: 12911 },
+    { ano: '2009', taxa: 7464 },  { ano: '2010', taxa: 7000 },  { ano: '2011', taxa: 6418 },
+    { ano: '2012', taxa: 4571 },  { ano: '2013', taxa: 5891 },  { ano: '2014', taxa: 5012 },
+    { ano: '2015', taxa: 6207 },  { ano: '2016', taxa: 7893 },  { ano: '2017', taxa: 6947 },
+    { ano: '2018', taxa: 7536 },  { ano: '2019', taxa: 10129 }, { ano: '2020', taxa: 11088 },
+    { ano: '2021', taxa: 13038 }
+];
+
+const anos = dadosProdes.map(item => item.ano);
+const taxas = dadosProdes.map(item => item.taxa);
+
 
 const ctx = document.getElementById('Grafico1').getContext('2d');
 const button = document.querySelector('.botao');
